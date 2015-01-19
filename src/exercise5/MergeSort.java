76830b2685c18b317b3de59f3b2f3dd921091ee1
unknown <cclayt02@CSPC044.dcs.bbk.ac.uk>
2015-01-19 21:00:38 +0000

    began implementing merge sort

---
+++ src/exercise5/MergeSort.java
@@ -0,0 +1,19 @@
+package exercise5;
+
+import java.util.ArrayList;
+
+public class MergeSort {
+	public static ArrayList<Integer> mergeSort(ArrayList<Integer> unsortedList){
+		ArrayList<Integer> sortedList = new ArrayList<Integer>();
+		if (unsortedList.size() < 2) return unsortedList;
+		else {
+			
+		}
+		return unsortedList;
+	}
+	
+	private ArrayList<Integer> sortSubList(){
+		
+		return null;
+	}
+}
