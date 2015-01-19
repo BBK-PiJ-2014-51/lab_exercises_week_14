814400cf07e2ef8bb6b265375053dc3ef4fa2e5a
unknown <cclayt02@CSPC044.dcs.bbk.ac.uk>
2015-01-19 20:43:30 +0000

    began implementing sorted list

---
+++ src/exercise4/SortedList.java
@@ -0,0 +1,18 @@
+package exercise4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortedList {
+	List<Integer> list;
+	
+	public SortedList(){
+		list = new ArrayList<Integer>();
+	}
+	
+	public SortedList(List list){
+		this.list = list;
+	}
+	
+
+}
